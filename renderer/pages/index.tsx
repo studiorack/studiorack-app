ed2824d16987c04878ad45cfb74945002c1da9a6
Kim T <kimturley@gmail.com>
2020-09-13 16:55:44 -0700

    Site updates

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -29,14 +29,14 @@ class Home extends Component<HomeProps, {
         <Head>
           <title>{siteTitle}</title>
         </Head>
-        <section className={`${styles.section} ${styles.sectionCreators}`}>
+        <section className={`${styles.section} ${styles.sectionOpenSource}`}>
           <div className={styles.container}>
             <div className={styles.inner}>
-              <img className={styles.sectionImage} src={`${this.state.router.basePath}/images/creators-mobile.jpg`} alt="Creators" />
-              <h2 className={styles.title}>Automate your plugin publishing workflow</h2>
-              <p>Our plugin starter template uses GitHub Actions to build your plugin with the Steinberg VST3 SDK, publishing a new version straight to Github Releases.</p>
-              <Link href={`${this.state.router.basePath}/docs`}>
-                <a className="button">Get started</a>
+              <img className={styles.sectionImage} src={`${this.state.router.basePath}/images/studio-audio-rack-mobile.jpg`} alt="Open source" />
+              <h2 className={styles.title}>An open-source audio plugin ecosystem</h2>
+              <p>Our tools are built using GitHub and open-source libraries, ensuring you will always be able to access them.</p>
+              <Link href={`${this.state.router.basePath}/plugins`}>
+                <a className="button">Browse plugins</a>
               </Link>
             </div>
           </div>
@@ -47,9 +47,17 @@ class Home extends Component<HomeProps, {
               <img className={styles.sectionImage} src={`${this.state.router.basePath}/images/producers-mobile.jpg`} alt="Producers" />
               <h2 className={styles.title}>Easy plugin installation &amp; management</h2>
               <p>Our app and command line tools allow you to search our plugin registry for free plugins. You can install and manage plugin versions all from one place.</p>
-              <Link href={`${this.state.router.basePath}/docs`}>
-                <a className="button">Get started</a>
-              </Link>
+              <a className="button" href="https://github.com/studiorack/studiorack-app/releases/latest" target="_blank">Download the app</a> <a className="button button-clear" href="https://www.npmjs.com/package/@studiorack/studiorack-cli" target="_blank">Install the CLI</a>
+            </div>
+          </div>
+        </section>
+        <section className={`${styles.section} ${styles.sectionCreators}`}>
+          <div className={styles.container}>
+            <div className={styles.inner}>
+              <img className={styles.sectionImage} src={`${this.state.router.basePath}/images/creators-mobile.jpg`} alt="Creators" />
+              <h2 className={styles.title}>Automate your plugin publishing workflow</h2>
+              <p>Our plugin starter template uses GitHub Actions to build your plugin with the Steinberg VST3 SDK, publishing a new version straight to Github Releases.</p>
+              <a className="button" href="https://github.com/studiorack/studiorack-plugin" target="_blank">Use the plugin template</a>
             </div>
           </div>
         </section>
