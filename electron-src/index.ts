51e3531386b28935ad06217ec2ce0d6fc18e5067
Kim T <kimturley@gmail.com>
2020-09-13 22:14:07 -0700

    Enable secure http header

--- electron-src/index.ts
+++ electron-src/index.ts
@@ -3,7 +3,7 @@ import { join } from 'path'
 import { format } from 'url'
 
 // Packages
-import { BrowserWindow, app, ipcMain, IpcMainEvent } from 'electron'
+import { BrowserWindow, app, session, ipcMain, IpcMainEvent } from 'electron'
 import isDev from 'electron-is-dev'
 import prepareNext from 'electron-next'
 
@@ -20,10 +20,29 @@ app.on('ready', async () => {
 
   app.dock.setIcon(join(__dirname, '../renderer/out/icons/icon.png'))
 
+  // enable more secure http header
+  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    callback({
+      responseHeaders: {
+        ...details.responseHeaders,
+        'Content-Security-Policy': [`
+          default-src 'self';
+          connect-src 'self' data:;
+          font-src 'self' fonts.gstatic.com;
+          img-src 'self' data: github.com *.s3.amazonaws.com;
+          media-src 'self' github.com *.s3.amazonaws.com;
+          script-src 'self' 'unsafe-inline' 'unsafe-eval';
+          style-src 'self' 'unsafe-inline' fonts.googleapis.com
+        `]
+      }
+    })
+  })
+
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
+      contextIsolation: true,
       nodeIntegration: false,
       preload: join(__dirname, 'preload.js'),
     },
@@ -40,9 +59,9 @@ app.on('ready', async () => {
   mainWindow.loadURL(url)
 
   // If developing locally, open developer tools
-  // if (isDev) {
+  if (isDev) {
     mainWindow.webContents.openDevTools()
-  // }
+  }
 })
 
 // Quit the app once all windows are closed
