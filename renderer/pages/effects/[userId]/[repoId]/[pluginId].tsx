d0749e5232b1e2a5185d682df4c15d9ed094926b
Kim T <kimturley@gmail.com>
2022-02-21 20:40:58 -0800

    Plurals in timeAgo function

--- renderer/pages/effects/[userId]/[repoId]/[pluginId].tsx
+++ renderer/pages/effects/[userId]/[repoId]/[pluginId].tsx
@@ -80,24 +80,39 @@ class PluginPage extends Component<
   timeSince(date: string) {
     const seconds = Math.floor((new Date().getTime() - new Date(date).getTime()) / 1000);
     let interval = seconds / 31536000;
-    if (interval > 1) {
+    if (interval > 2) {
       return Math.floor(interval) + ' years';
     }
-    interval = seconds / 2592000;
     if (interval > 1) {
+      return Math.floor(interval) + ' year';
+    }
+    interval = seconds / 2592000;
+    if (interval > 2) {
       return Math.floor(interval) + ' months';
     }
-    interval = seconds / 86400;
     if (interval > 1) {
+      return Math.floor(interval) + ' month';
+    }
+    interval = seconds / 86400;
+    if (interval > 2) {
       return Math.floor(interval) + ' days';
     }
-    interval = seconds / 3600;
     if (interval > 1) {
+      return Math.floor(interval) + ' day';
+    }
+    interval = seconds / 3600;
+    if (interval > 2) {
       return Math.floor(interval) + ' hours';
     }
-    interval = seconds / 60;
     if (interval > 1) {
+      return Math.floor(interval) + ' hour';
+    }
+    interval = seconds / 60;
+    if (interval > 2) {
       return Math.floor(interval) + ' minutes';
+    }
+    if (interval > 1) {
+      return Math.floor(interval) + ' minute';
     }
     return Math.floor(seconds) + ' seconds';
   }
